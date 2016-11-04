049b23752e9733b31da725d93aff318166b07741
Devon Koch <devon.r.koch@gmail.com>
2016-11-03 17:05:10 -0700

    Move first component to /containers, add react-router in config/routes

---
+++ app/config/routes.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var browserHistory = ReactRouter.browserHistory;
+var IndexRoute = ReactRouter.IndexRoute;
+var Main = require('../containers/Main');
+
+var routes = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+
+    </Route>
+  </Router>
+);
+
+module.exports = routes;
